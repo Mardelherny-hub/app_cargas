488bc2d771e7be078b73ea401c089d5dac9d7528
Victor Alcalde <79488170+Mardelherny-hub@users.noreply.github.com>
2026-09-14 12:44:15 -0300

    fix: bind shipment item compatibility controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\Company\ShipmentItemController;
+use App\Http\Controllers\Company\ShipmentItemControllerCompat;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            ShipmentItemController::class,
+            ShipmentItemControllerCompat::class
+        );
     }
 
     /**
