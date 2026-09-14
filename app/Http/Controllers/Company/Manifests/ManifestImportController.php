bc5ded3895174610e3e8e60b2b1b597f4a0adb0f
Victor Alcalde <79488170+Mardelherny-hub@users.noreply.github.com>
2026-09-14 12:30:15 -0300

    feat: accept bill date during manifest import

--- app/Http/Controllers/Company/Manifests/ManifestImportController.php
+++ app/Http/Controllers/Company/Manifests/ManifestImportController.php
@@ -80,6 +80,7 @@ class ManifestImportController extends Controller
             'departure_date' => 'nullable|date',
             'loading_date' => 'nullable|date',
             'discharge_date' => 'nullable|date',
+            'bill_date' => 'nullable|date',
             'voyage_number' => 'nullable|string|max:100',
         ], [
             'manifest_file.required' => 'Debe seleccionar un archivo para importar.',
@@ -144,7 +145,8 @@ class ManifestImportController extends Controller
                 $request->input('departure_date'),
                 $request->input('voyage_number'),
                 $request->input('loading_date'),
-                $request->input('discharge_date')
+                $request->input('discharge_date'),
+                $request->input('bill_date')
             );
 
             Log::info('Manifest import encolado', [
